a4a21c7bdbb22bb7c961542383da5d21d908fdc2
Angelina <angelaaquin13@gmail.com>
2024-08-09 23:19:24 +0000

    initial

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+
+let pronouns = ["the", "our", "my"];
+let adjs = ["great", "big", "fancy"];
+let nouns = ["jogger", "racoon", "flower"];
+let sites = [".com", ".net", ".gov"];
+
+var chosenPronoun = "";
+var chosenAdj = "";
+var chosenNoun = "";
+var chosenSite = "";
+
+for (let pn in pronouns) {
+  chosenPronoun = pronouns[pn];
+  for (let ad in adjs) {
+    chosenAdj = adjs[ad];
+    for (let n in nouns) {
+      chosenNoun = nouns[n];
+      for (let s in sites) {
+        chosenSite = sites[s];
+
+        console.log(`${chosenPronoun}${chosenAdj}${chosenNoun}${chosenSite}`);
+      }
+    }
+  }
+}
